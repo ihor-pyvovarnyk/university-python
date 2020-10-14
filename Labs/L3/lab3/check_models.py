fe31f81fdb0bfb1df82ec637128751a8f25a00d2
ihor-pyvovarnyk <igor.pivovarnik@gmail.com>
2020-10-14 05:41:40 +0300

    Рефакторінг Лаб3

--- Labs/L3/lab3/check_models.py
+++ Labs/L3/lab3/check_models.py
@@ -1,8 +1,8 @@
-from models import Session, Users, Wallets, Transactions
+from lab3.models import Session, Users, Wallets, Transactions
 
 session = Session()
 
-user = Users(uid=1, email="example@example.com", first_name="First", last_name="Last")
+user = Users(uid=1, email="example@example.com", password=b'', first_name="First", last_name="Last")
 wallet_1 = Wallets(uid=1, name="My wallet", funds=90, owner=user)
 wallet_2 = Wallets(uid=2, name="My wallet", funds=110, owner=user)
 transaction = Transactions(uid=1, from_wallet=wallet_1, to_wallet=wallet_2, amount=10)
